cefabe427b4912d3426bb3539702bd6be4e717a9
johngschulz <schulz.johng@gmail.com>
2016-09-29 15:44:12 -0500

    Added getDistanceFromSelf method

--- java-track/exercises/Robot.java
+++ java-track/exercises/Robot.java
@@ -2,6 +2,7 @@
 public class Robot {
 
 	//Fields: The robot has 5 fields
+	
 	private String name;
 	private int positionx;
 	private int positiony;
@@ -14,10 +15,7 @@ public class Robot {
 	//Get the position
 	//Get the speed
 	//Get the orientation
-	//Change speed
-	//Rotate orientation by +-90 degrees
-	//Get distance from other robot
-	//Display information as a String
+	
 	
 	//Create a robot (constructor)
 	public Robot(String name, int positionx,int positiony, int speed, int orientation)
@@ -49,10 +47,12 @@ public class Robot {
 		return this.orientation;
 	}
 	public void changeSpeed(int newSpeed)
+	//Change speed
 	{
 		this.speed = this.speed + newSpeed;
 	}
 	public void turnRobot(int newOrientation)
+	//Rotate orientation by +-90 degrees
 	{
 		if(newOrientation % 90 == 0)
 		{
@@ -87,6 +87,7 @@ public class Robot {
 
 
 	public static double getDistance(Robot robot1, Robot robot2)
+	//Get distance between Robots
 	{
 		int x1 = robot1.getPositionx();
 		int y1 = robot1.getPositiony();
@@ -95,10 +96,20 @@ public class Robot {
 		double distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
 		return distance;		
 	}
+	public void getDistanceFromSelf(Robot otherRobot)
+	{
+		int x1 = this.positionx;
+		int y1 = this.positiony;
+		int x2 = otherRobot.getPositionx();
+		int y2 = otherRobot.getPositiony();
+		
+		double distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		System.out.println(this.name + " is "+ distance+" units from " + otherRobot.getName());
+	}
 
 	
 	public String toString()
-
+	//Display information as a String
 	{
 		return " name: " + this.name + " | " + " position: (" + this.positionx + " , " + this.positiony + ") " +" | " + " speed: " + this.speed + "  | " +" orientation: " + this.orientation;
 	}
@@ -113,6 +124,7 @@ public class Robot {
 		yourRobot.turnRobot(360);
 		System.out.println(yourRobot);
 		System.out.println(getDistance(myRobot,yourRobot));
+		myRobot.getDistanceFromSelf(yourRobot);
 	}
 }
 
